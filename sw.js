a2ec096efdf1a08d40047d2c9d6c3534647291b8
GuC-13 <clement.guerra@sbl.ch>
2026-09-24 15:33:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "schreibraum-cache-v4";
+const CACHE_NAME = "schreibraum-cache-v5";
 const FILES_TO_CACHE = ["./", "./index.html"];
 
 self.addEventListener("install", (event) => {
